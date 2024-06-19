ab75493c8966e8060e40e0fac6254c42580e3e1a
YU Zhejian <Zhejian.19@intl.zju.edu.cn>
2024-06-19 22:34:48 +0800

    Small bugs in Makefile fixed

--- ystr_core/src/main/java/com/github/yu_zhejian/ystr/io/TwoBitParser.java
+++ ystr_core/src/main/java/com/github/yu_zhejian/ystr/io/TwoBitParser.java
@@ -305,11 +305,9 @@ public final class TwoBitParser implements AutoCloseable {
     public byte @NotNull [] getSequence(
             final int seqID, final int start, final int end, boolean parseMasks)
             throws IOException {
-        if (start == end) {
-            return new byte[0];
-        }
-        loadSeqInfo(seqID);
+        // Check whether `start == end` will impact performance, so
         StrUtils.ensureStartEndValid(start, end, dnaSizes[seqID]);
+        loadSeqInfo(seqID);
 
         // Number of bases to read at the end.
         var retLen = end - start;
